9b6ed5311e27ca603cb0156567f4252a3d91307b
Igor Libukin <ulterpwnz@list.ru>
2022-04-07 22:22:04 +0300

    Request and Response

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ $posts = [
     ],
 ];
 Route::get('/posts', function () use($posts){
+//    dd(request()->all());
+    dd((int)request()->query('page', 1));
    return view('posts.index', ['posts'=>$posts]);
 });
 Route::get('/posts/{id}', function ($id) use($posts){
@@ -40,4 +42,32 @@ Route::get('/posts/{id}', function ($id) use($posts){
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Posts from' . $daysAgo . 'days ago';
-})->name('posts.recent.index');
+})->name('posts.recent.index')->middleware('auth');
+
+
+
+Route::prefix('/fun')->name('fun.')->group(function () use($posts){
+    Route::get('/fun/responses', function () use($posts){
+        return response($posts, 201)
+            ->header('Content-Type', 'application/json')
+            ->cookie('MY_COOKIE', 'Piotr Jura', 3600);
+    })->name('responses');
+    Route::get('/fun/redirect', function (){
+        return redirect('/contact');
+    });
+    Route::get('/fun/back', function (){
+        return back();
+    })->name('back');
+    Route::get('/fun/named-route', function (){
+        return redirect()->route('posts.show', ['id'=>1]);
+    })->name('named-route');
+    Route::get('/fun/away', function (){
+        return redirect()->away('https://www.google.com/');
+    })->name('away');
+    Route::get('/fun/json', function () use($posts){
+        return response()->json($posts);
+    })->name('json');
+    Route::get('/fun/download', function () use($posts){
+        return response()->download(public_path('/daniel.jpg'), 'face.jpg');
+    })->name('download');
+});
