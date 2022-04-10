c7dc2fb0a5d20c08e76822d3e73c82c817ca2793
Igor Libukin <ulterpwnz@list.ru>
2022-04-10 19:28:42 +0300

    Assets & Styling

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,13 +1,19 @@
-<div><input type="text" name="title" value="{{old('title', optional($post ?? null)->title)}}"></div>
+<div class="form-group">
+    <label for="title">Title</label>
+    <input id='title' type="text" name="title" class="form-control" value="{{old('title', optional($post ?? null)->title)}}">
+</div>
 @error('title')
-<div>{{$message}}</div>
+<div class="alert alert-danger">{{$message}}</div>
 @enderror
-<div><textarea type="text" name="content">{{old('content', optional($post ?? null)->content)}}</textarea></div>
+<div class="form-group">
+    <label for="content">Content</label>
+    <textarea class="form-control" id="content" type="text" name="content">{{old('content', optional($post ?? null)->content)}}</textarea>
+</div>
 @if($errors->any())
-    <div>
-        <ul>
+    <div class="mb-3">
+        <ul class="list-group">
             @foreach($errors->all() as $error)
-                <li>
+                <li class="list-group-item list-group-item-danger">
                     ${{$error}}
                 </li>
             @endforeach
--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,14 +1,10 @@
-{{--        @break($key = 2)--}}
-{{--        @continue($key = 1)--}}
-@if($loop->even)
-    <div>{{$key}}.{{$post->title}}</div>
-@else
-    <div style="background-color: silver">{{$key}}.{{$post->title}}</div>
-@endif
-<div>
-    <form action="{{route('posts.destroy', ['post'=>$post->id])}}" method="POST">
+<h3><a href="{{rout('posts.show', ['post'=>$post->id])}}"></a></h3>
+<div style="background-color: silver">{{$key}}.{{$post->title}}</div>
+<div class="mb-3">
+    <a href="{{rout('posts.edit', ['post'=>$post->id])}}" class="btn btn-primary">Edit</a>
+    <form class="d-inline" action="{{route('posts.destroy', ['post'=>$post->id])}}" method="POST">
         @csrf
         @method("DELETE")
-        <input type="submit" value="Delete!">
+        <input type="submit" value="Delete!" class="btn btn-primary">
     </form>
 </div>
