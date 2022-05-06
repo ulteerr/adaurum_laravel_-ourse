6448ced4b366a71f730fe645847425e2326630fc
Igor Libukin <i.libukin@adaurum.ru>
2022-05-06 17:38:25 +0300

    Many to Many Polymorphic Eloguent

--- app/Http/Controllers/UserCommentController.php
+++ app/Http/Controllers/UserCommentController.php
@@ -2,20 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\BlogPost;
 use App\Http\Requests\StoreComment;
+use App\User;
 
-class PostCommentController extends Controller
+class UserCommentController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth')->only(['store']);
     }
 
-    public function store(BlogPost $post, StoreComment $request)
+    public function store(User $user, StoreComment $request)
     {
-        // Comment::create()
-        $post->comments()->create([
+        $user->commentsOn()->create([
             'content' => $request->input('content'),
             'user_id' => $request->user()->id,
         ]);
--- resources/views/components/comment-form.blade.php
+++ resources/views/components/comment-form.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-2 mt-2">
     @auth
-        <form method="POST" action="{{ route('posts.comments.store', ['post' => $post->id]) }}">
+        <form method="POST" action="{{ $route }}">
             @csrf
 
             <div class="form-group">
@@ -13,5 +13,5 @@
     @else
         <a href="{{ route('login') }}">Sign-in</a> to post comments!
     @endauth
-</div>
-<hr/>
+    </div>
+    <hr/>
