c39f1eb7f74675d99f903ddeed155c4b7e122f91
Igor Libukin <ulterpwnz@list.ru>
2022-06-01 21:34:47 +0300

    #45_283

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,8 +24,9 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Request $request, $id)
     {
+        // $request->session()->reflash();
         return view('posts.show', ['post' => BlogPost::findOrFail($id)]);
     }
 
@@ -36,9 +37,18 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        $title = $request->input('title', 'Draft title');
-        $content = $request->input('content', 'Draft content');
+        $validatedData = $request->validate([
+            'title' => 'required|max:100',
+            'content' => 'required'
+        ]);
 
-        dd($title, $content);
+        $blogPost = new BlogPost();
+        $blogPost->title = $request->input('title');
+        $blogPost->content = $request->input('content');
+        $blogPost->save();
+
+        $request->session()->flash('status', 'Blog post was created!');
+        
+        return redirect()->route('posts.show', ['post' => $blogPost->id]);
     }
 }
